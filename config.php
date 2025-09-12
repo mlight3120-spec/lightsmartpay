173de6ced769065f71c4048d2c55437ebe143034
mlight3120-spec <mlight3120@gmail.com>
2025-09-12 19:09:21 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'DB_HOST' => getenv('dpg-d300uhqli9vc738gsp3g-aT'),
+    'DB_PORT' => getenv('5432'),
+    'DB_NAME' => getenv('lightsmarttopup'),
+    'DB_USER' => getenv('lightsmarttopup_user'),
+    'DB_PASS' => getenv('Yb68Pt3nySr1IRpuX7BOcEjmWrrmbNcJ')
+];
