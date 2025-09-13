bbe66452d3089edef5b448c33f33739e435f72cd
mlight3120-spec <mlight3120@gmail.com>
2025-09-13 13:31:19 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'DB_HOST' => getenv('dpg-d300uhqli9vc738gsp3g-aT'),
+    'DB_HOST' => getenv('dpg-d326jbadbo4c73a6urog-a'),
     'DB_PORT' => getenv('5432'),
-    'DB_NAME' => getenv('lightsmarttopup'),
-    'DB_USER' => getenv('lightsmarttopup_user'),
-    'DB_PASS' => getenv('Yb68Pt3nySr1IRpuX7BOcEjmWrrmbNcJ')
+    'DB_NAME' => getenv('lightsmartpay'),
+    'DB_USER' => getenv('lightsmartpay_user'),
+    'DB_PASS' => getenv('ikPevBhb9xHModBeJYsGIvtcmY6rmPuH')
 ];
