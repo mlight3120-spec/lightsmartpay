49e969c1e8e385d964b7f75cbfa24cd98d1f05a9
mlight3120-spec <mlight3120@gmail.com>
2025-09-18 19:28:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,8 @@
 <?php
-// config.php
-
-$host = "dpg-d326jbadbo4c73a6urog-a";
-$port = "5432";
-$dbname = "lightsmartpay";
-$user = "lightsmartpay_user";
-$pass = "ikPevBhb9xHModBeJYsGIvtcmY6rmPuH";
-
-try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-} catch (PDOException $e) {
-    die("❌ DB Connection failed: " . $e->getMessage());
-}
+return [
+    "DB_HOST" => "dpg-d326jbadbo4c73a6urog-a",  
+    "DB_PORT" => "5432",                     
+    "DB_NAME" => "lightsmartpay",             
+    "DB_USER" => "lightsmartpay_user",        
+    "DB_PASS" => "ikPevBhb9xHModBeJYsGIvtcmY6rmPuH"         
+];
